e7651b846d3d4c765976942f81d3d5b86191560e
ouckah <aidan.ouckama@gmail.com>
2024-12-22 20:54:57 -0500

    basic rigidbody

---
+++ rigidbody.go
@@ -0,0 +1,21 @@
+package physicsenggo
+
+/*
+	A RigidBody knows its current velocity and position. See the RigidBody methods getPosition(), getAngle(), getVelocity().
+	A RigidBody can calculate its translational and rotational energy and momentum.
+	See translationalEnergy(), rotationalEnergy(), momentum().
+
+	@see https://www.myphysicslab.com/develop/docs/Engine2D.html#themathbehindthephysicsengine
+*/
+
+type RigidBody struct {
+	Velocity, Position Vector2
+}
+
+func (body RigidBody) GetPosition() Vector2 {
+	return body.Position
+}
+
+func (body RigidBody) GetVelocity() Vector2 {
+	return body.Velocity
+}

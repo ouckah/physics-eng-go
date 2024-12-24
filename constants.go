a33b6bcfa55e34bdda637cc1619d4ebee279775d
ouckah <aidan.ouckama@gmail.com>
2024-12-23 20:06:08 -0500

    adjust gravity

--- constants.go
+++ constants.go
@@ -5,6 +5,6 @@ var SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
 
 // simulation constants
 var ELASTICITY = 0.8
-var GRAVITY = Vector2{X: 0, Y: 0.1}
+var GRAVITY = Vector2{X: 0, Y: 0.25}
 var DELTA_TIME = 1.
 var BUFFER = 0.1
